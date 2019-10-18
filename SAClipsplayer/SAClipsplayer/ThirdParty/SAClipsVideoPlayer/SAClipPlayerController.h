83f5f5e1738893d58ae23d00602942fa01a63e1c
helantage <851962032@qq.com>
2019-10-18 15:45:19 +0800

    feat:建立controller

---
+++ SAClipsplayer/SAClipsplayer/ThirdParty/SAClipsVideoPlayer/SAClipPlayerController.h
@@ -0,0 +1,17 @@
+//
+//  SAClipPlayerController.h
+//  SAClipsplayer
+//
+//  Created by zheng on 2019/10/18.
+//  Copyright © 2019 kpIng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SAClipPlayerController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SAClipsplayer/SAClipsplayer/ThirdParty/SAClipsVideoPlayer/SAClipPlayerController.m
@@ -0,0 +1,24 @@
+//
+//  SAClipPlayerController.m
+//  SAClipsplayer
+//
+//  Created by zheng on 2019/10/18.
+//  Copyright © 2019 kpIng. All rights reserved.
+//
+
+#import "SAClipPlayerController.h"
+
+@interface SAClipPlayerController ()
+
+@end
+
+@implementation SAClipPlayerController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+
+
+@end
